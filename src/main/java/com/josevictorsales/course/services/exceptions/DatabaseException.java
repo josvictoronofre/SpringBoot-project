ccf1e911040151f74ebd0a83fc78f573809f4d55
Victor <josvictoronofre@gmail.com>
2024-03-27 11:11:07 -0300

    Exception treatment -> delete

---
+++ src/main/java/com/josevictorsales/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.josevictorsales.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
